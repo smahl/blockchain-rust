fcf8f82f3bbb89449a2c77f4962f517d8ccbf25f
smahl <smahlhanssen@gmail.com>
2022-02-20 14:11:25 +0100

    Mining

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use blockchainlib::*;
 fn main () {
-    let mut block = Block::new(13,12345,vec![0; 32], 1, "Genesis block!".to_owned());
+    let mut block = Block::new(13,12345,vec![0; 32], 0, "Genesis block!".to_owned(), 0x000fffffffffffffffffffffffffffff);
 
     println!("{:?}", &block);
 
     let h = block.hash();
+
+    block.mine();
 
     println!("{:?}", &h);
 
